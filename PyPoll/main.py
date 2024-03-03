70cab82a886f7e1d9c31783d56c91ac5db5cfa24
dragatuso <danielaragatuso@gmail.com>
2024-03-03 16:31:06 -0600

    Create a list with the candidates' names

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -14,6 +14,11 @@ csvpath = os.path.join('PyPoll','Resources/election_data.csv')
 #Define a counter for total number of rows to calculate total votes
 vote_count = 0
 
+#Create a list for storing the cadidates' names
+candidates = []
+
+
+
 #Open the CSV file
 with open(csvpath,'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
@@ -26,6 +31,16 @@ with open(csvpath,'r') as csvfile:
     for row in csvreader:
         #Count the number of rows which are equivalent to the number of votes
         vote_count = vote_count + 1
+
+        #Fill the list with the name of the candidates every time we loop through a new name, but only if that name is not already part of the list
+        if candidates.count(row[2]) < 1:
+           candidates.append(row[2])
+
+                
+    #print(candidates)
+                
+                
+
 
     
     #Print total votes, the line formats and empty rows below

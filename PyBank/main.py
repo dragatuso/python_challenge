f0705c8edd8bc184dabb43cef5956727e86ea387
dragatuso <danielaragatuso@gmail.com>
2024-03-03 12:24:45 -0600

    Calculation of net total amount profit/losses

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,10 +12,12 @@ csvpath = os.path.join('PyBank','budget_data.csv')
 #Define a counter for total number of rows to calculate total months
 row_count = 0
 
+#Define a sum for total profits/losses
+total_profit_losses = 0
+
 #Open the CSV file
 with open(csvpath,'r') as csvfile:
-    csvreader = csv.reader(csvfile)
-
+    csvreader = csv.reader(csvfile, delimiter=",")
     #Loop thorugh the rows in the CSV file and count the number of rows
     for row in csvreader:
         row_count = row_count + 1
@@ -26,14 +28,21 @@ with open(csvpath,'r') as csvfile:
     #Print the Total Months
     print("Total Months: " + str(total_months))
 
-
-
 #Open the CSV file
-# with open(csvpath,'r') as csvfile:
-#     csvreader = csv.reader(csvfile, delimiter=',')
+with open(csvpath,'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
     
-    #Read all rows and print them
-    # for row in csvreader:
-    #     print(row)
-
-
+    #Read the header row first
+    csv_header = next(csvreader)
+    #print(f"CSV Header: {csv_header}")
+    
+    #Read each row of data after the header
+    for row in csvreader:
+        #print(row)
+        #sum the values of profits and losses in each row and accumulate it in the total_profits_losses variable defined
+        total_profit_losses = total_profit_losses + int(row[1])
+    
+    #Print the net total amount of profits/losses
+    print("Total: " + "$" + str(total_profit_losses))
+    
+    
